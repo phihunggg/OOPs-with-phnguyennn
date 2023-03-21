70550f9f67ff751a0922f3982101e2d11f07f6ab
Hung Nguyen <Hung Nguyen@PhiHung>
2023-03-21 20:14:30 +0700

    add constructor demo code

---
+++ src/pratice/Constructor.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pratice;
+
+/**
+ *
+ * @author Hung Nguyen
+ */
+public class Constructor {
+    private String name;
+    private int age;
+
+    
+    //constructor mặc định
+    public Constructor() {
+    }
+
+    
+    //constructor có tham số
+    public Constructor(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    
+    
+}
